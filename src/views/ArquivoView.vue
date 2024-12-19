39e62da0766dbc6bce0fa2b90f522ff55baa48d9
ratto <rattopedro@gmail.com>
2024-12-19 20:24:48 -0300

    Registro Detalhe, form and table, added

--- src/views/ArquivoView.vue
+++ src/views/ArquivoView.vue
@@ -3,6 +3,7 @@ import { ref, useTemplateRef } from 'vue';
 import {
   VTextField,
   VForm,
+  VTable,
   VRow,
   VCol,
   VSelect,
@@ -16,11 +17,36 @@ import { Rcb001, RegistroDetalhe } from '../types/Rcb001';
 import { FormaRecebimento, MeioArrecadacao } from '../enums/Rcb001Enums';
 
 const formArquivo = useTemplateRef('form-arquivo');
-const arquivo = ref<Rcb001>({} as Rcb001);
+const arquivo = ref<Rcb001>({ registrosDetalhe: [] as RegistroDetalhe[] } as Rcb001);
 const registroDetalhe = ref<RegistroDetalhe>({ bancoCreditado: {} } as RegistroDetalhe);
 
 const meiosArrecadacao = [MeioArrecadacao.CAIXA, MeioArrecadacao.ELETRONICA, MeioArrecadacao.INTERNET];
 const formasRecebimento = [FormaRecebimento.CHEQUE, FormaRecebimento.DINHEIRO, FormaRecebimento.NAO_IDENTIFICADA];
+
+function cancelRegistroDetalhe() {
+  registroDetalhe.value = { bancoCreditado: {} } as RegistroDetalhe;
+}
+
+function deleteRegistroDetalhe() {
+  // To be implemented
+}
+
+function resetArquivo() {
+  arquivo.value.registrosDetalhe = [] as RegistroDetalhe[];
+}
+
+async function addRegistroDetalhe() {
+  const formIsValid = await formArquivo.value?.validate();
+
+  if (formIsValid?.valid) {
+    arquivo.value.registrosDetalhe.push(registroDetalhe.value);
+    registroDetalhe.value = { bancoCreditado: {} } as RegistroDetalhe;
+  }
+}
+
+async function editRegistroDetalhe() {
+  // To be implemented
+}
 
 async function submit() {
   const formIsValid = await formArquivo.value?.validate();
@@ -32,7 +58,7 @@ async function submit() {
 <template>
   <div id="arquivo-view">
     <h1>Leiaute RCB001</h1>
-    <VForm novalidate ref="form-arquivo" @submit.prevent="submit">
+    <VForm novalidate ref="form-arquivo" @submit.prevent="submit" @reset="resetArquivo">
       <VRow>
         <VCol>
           <VTextField
@@ -105,7 +131,7 @@ async function submit() {
           <VExpansionPanelText>
             <VRow>
               <VCol>
-                <VTextField :model-value="registroDetalhe.seqRegistro" disabled>{{
+                <VTextField v-model="registroDetalhe.seqRegistro" disabled>{{
                   !!arquivo.registrosDetalhe ? arquivo.registrosDetalhe.length + 1 : '1'
                 }}</VTextField>
               </VCol>
@@ -217,9 +243,47 @@ async function submit() {
                 />
               </VCol>
             </VRow>
+            <div class="form-footer d-flex justify-end mt-3">
+              <VBtn type="button" @click="cancelRegistroDetalhe" color="secondary" class="mx-1">Cancelar</VBtn>
+              <VBtn type="button" @click="addRegistroDetalhe" color="primary" class="mx-1">Adicionar Registro</VBtn>
+            </div>
           </VExpansionPanelText>
         </VExpansionPanel>
       </VExpansionPanels>
+
+      <div class="my-4">
+        <VTable v-if="!!arquivo.registrosDetalhe && arquivo.registrosDetalhe.length > 0">
+          <thead>
+            <tr>
+              <th>Registro</th>
+              <th>Agência Creditada</th>
+              <th>Conta Corrente</th>
+              <th>Data do Pagamento</th>
+              <th>Data do Crédito</th>
+              <th>Valor Recebido</th>
+              <th>Valor da Tarifa</th>
+              <th>Agência Recebedora</th>
+              <th>Meio de Arrecadação</th>
+              <th>Forma de Recebimento</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(registro, index) in arquivo.registrosDetalhe" :key="index">
+              <td>{{ registro.seqRegistro }}</td>
+              <td>{{ `${registro.bancoCreditado.prefixoAgencia}-${registro.bancoCreditado.dvAgencia}` }}</td>
+              <td>{{ `${registro.bancoCreditado.numConta}-${registro.bancoCreditado.dvConta}` }}</td>
+              <td>{{ registro.dataPagamento }}</td>
+              <td>{{ registro.dataCredito }}</td>
+              <td>{{ registro.valorRecebido }}</td>
+              <td>{{ registro.valorTarifa }}</td>
+              <td>{{ registro.prefAgenciaRecebedora }}</td>
+              <td>{{ registro.meioArrecadacao }}</td>
+              <td>{{ registro.formaRecebimento }}</td>
+            </tr>
+          </tbody>
+        </VTable>
+        <VAlert v-else class="text-center" color="warning" text="Não existem registros a serem visualizados!" />
+      </div>
 
       <div class="form-footer d-flex justify-end mt-3">
         <VBtn type="reset" color="warning" class="mx-1">Limpar tudo</VBtn>
