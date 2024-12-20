ed8b6ee74edab81a8db815fad493addc8e379207
ratto <rattopedro@gmail.com>
2024-12-19 22:45:04 -0300

    Localization (pt) added

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,34 @@ import 'vuetify/_styles.scss';
 import { router } from './router';
 import App from './App.vue';
 import './style.scss';
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n';
+import { createI18n, useI18n } from 'vue-i18n';
+import { pt } from 'vuetify/locale';
 
 const pinia = createPinia();
 
+const messages = {
+  pt: {
+    $vuetify: {
+      ...pt,
+      confirmEdit: {
+        ok: 'Ok',
+        cancel: 'Cancelar',
+      },
+    },
+  },
+};
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'pt',
+  messages,
+});
+
 const vuetify = createVuetify({
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n }),
+  },
   theme: {
     defaultTheme: 'dark',
   },
@@ -19,4 +43,4 @@ const vuetify = createVuetify({
   directives,
 });
 
-createApp(App).use(router).use(pinia).use(vuetify).mount('#app');
+createApp(App).use(router).use(pinia).use(vuetify).use(i18n).mount('#app');
