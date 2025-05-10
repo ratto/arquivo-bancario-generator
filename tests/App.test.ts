4a18b85cae65ef56913a0a6c2ae5e2cf507e9823
ratto <rattopedro@gmail.com>
2025-05-10 17:49:41 -0300

    test: testes unitários para App.vue

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -1,5 +1,13 @@
-import { expect, it } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
+import { shallowMount } from '@vue/test-utils';
+import App from '../src/App.vue';
 
-it('should test positive', () => {
-  expect(true).toBeTruthy;
+describe('App.vue test suite', () => {
+  const sut = App;
+
+  test('renders the component correctly', () => {
+    const wrapper = shallowMount(sut);
+
+    expect(wrapper.exists()).toBe(true);
+  });
 });
